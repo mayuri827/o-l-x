903020e7e124c32bb2fdf5b129d56c652f5e875f
mayuri khade <mayurikhade85@gmail.com>
2024-07-27 09:10:10 +0530

    email update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,5 +13,5 @@ exports.verifyUserEmail = asyncHandler(async (req, res) => {
         message: `<p>your otp ${otp}</p>`
     })
 
-    res.json({ message: "Verification Send Success" })
+    res.json({ message: "Verification Send Success", isSend })
 })
